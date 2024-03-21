8603ffd6c162a868177f600d2c74f78e824392fa
zaragood <azaragoodness52@gmail.com>
2024-03-21 23:08:19 +0000

    testing

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
+"""imported sys, markdown"""
+
+
 import sys
 import markdown
 
 
 def markdown_to_html(readme_md, readme_html):
+    """
+    Converts a Markdown file to HTML.
+
+    Args:
+        readme_md (str): The path to the input Markdown file.
+        readme_html (str): The path to the output HTML file.
+
+    Raises:
+        FileNotFoundError: If the input Markdown file does not exist.
+    """
     try:
         with open(readme_md, 'r', encoding='utf-8') as md_file:
             markdown_content = md_file.read()
+            
+        html_content = markdown.markdown(markdown_content)
         with open(readme_html, 'w', encoding='utf-8') as html_file:
-            html_file.write(markdown_content)
+            html_file.write(html_content)
     except FileNotFoundError:
         print(f"Missing {readme_md}", file=sys.stderr)
         sys.exit(1)
