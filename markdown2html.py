e330ca19b9418eb370564151628c99b03ffdb051
zaragood <azaragoodness52@gmail.com>
2024-06-24 16:34:20 +0100

    html markdown

--- markdown2html.py
+++ markdown2html.py
@@ -1,40 +1,24 @@
-#!/usr/bin/python3
-"""imported sys
-    markdown
-"""
+#!/usr/bin/env python3
 import sys
 import markdown
 
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    sys.exit(1)
 
-def markdown_to_html(readme_md, readme_html):
-    """
-    Converts a Markdown file to HTML.
+input_file = sys.argv[1]
+output_file = sys.argv[2]
 
-    Args:
-        readme_md (str): The path to the input Markdown file.
-        readme_html (str): The path to the output HTML file.
+try:
+    with open(input_file, 'r') as f:
+        markdown_text = f.read()
+except FileNotFoundError:
+    print(f"Missing {input_file}", file=sys.stderr)
+    sys.exit(1)
 
-    Raises:
-        FileNotFoundError: If the input Markdown file does not exist.
-    """
-    try:
-        with open(readme_md, 'r', encoding='utf-8') as md_file:
-            markdown_content = md_file.read()    
-        html_content = markdown.markdown(markdown_content)
-        with open(readme_html, 'w', encoding='utf-8') as html_file:
-            html_file.write(html_content)
-    except FileNotFoundError:
-        print(f"Missing {readme_md}", file=sys.stderr)
-        sys.exit(1)
-    else:
-        sys.exit(0)
+html_text = markdown.markdown(markdown_text)
 
+with open(output_file, 'w') as f:
+    f.write(html_text)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py README.md README.html")
-        sys.exit(1)
-    else:
-        readme_md = sys.argv[1]
-        readme_html = sys.argv[2]
-        markdown_to_html(readme_md, readme_html)
+sys.exit(0)
